cd8a39f9c54a96aa72b00a9f8e4f6d09c2650446
lecs0 <szvetekaron@turr.hu>
2026-03-17 13:25:19 +0100

    romain

--- popup.js
+++ popup.js
@@ -1,29 +1,74 @@
-const ads = [
-    { text: "FREE DOWNLOAD NOW!", link: "https://youtu.be/psha3ecS5K8", image: "imgs/ad1.png" },
-    { text: "Click here to WIN BIG!", link: "https://youtu.be/psha3ecS5K8", image: "imgs/ad2.png" },
-    { text: "Hurry! BUDAPEST NYUGATI PÁLYAUDVAR GAMEPLAY 2026!", link: "https://youtu.be/mZoHGuK_Psg", image: "imgs/ad3.png" },
-    { text: "Jeffrey meghivott a szigetére! Klikk az elfogadáshoz", link: "https://youtu.be/yw_LlM6s5aI", image: "imgs/ad4.jpg"},
-    { text: "NV Casino 400.000,000Ft bónusz és 400 ingyen pörgetés. Játssz most!", link: "https://www.youtube.com/watch?v=yUqFpPj7jOs", image: "imgs/nvcasino-2-6814b768bc19b7.60142442.webp"}
-];
+const openPopup = document.getElementById('openPopup');
+const closePopup = document.getElementById('closePopup');
+const popup = document.getElementById('popup');
+const overlay = document.getElementById('gif-overlay');
+const img = document.getElementById('gif-image');
+const gifUrl = 'imgs/foxy-jumpscare.gif';
+let timeoutId = null;
 
 
-function getRandomAd() {
-    const randomIndex = Math.floor(Math.random() * ads.length);
-    return ads[randomIndex];
-}
 
-function loadAd() {
-    const ad = getRandomAd();
-    const adContainer = document.getElementById("ad-container");
-    if (adContainer) {
-        adContainer.innerHTML = `
-            <a href="${ad.link}" target="_blank">
-                <img src="imgs/small_x_icon_212667.png" alt="x" class="close-btn">
-                <img src="${ad.image}" alt="Ad">
-                <p>${ad.text}</p>
-            </a>
-        `;
+
+
+function getRandomPercentage(perc) {
+    const ranum = Math.floor(Math.random() * 10);
+    if (ranum < perc) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-document.addEventListener("DOMContentLoaded", loadAd);
+
+
+function showPopup() {
+    if (getRandomPercentage(2)) { // 20% esély a popup megjelenésére) {
+        popup.style.display = 'flex';
+    } else {
+        console.log("No popup this time!");
+    }
+
+};
+
+popup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
+
+
+
+
+
+
+function showGif(){
+        img.src = gifUrl;
+        overlay.style.display = 'flex';
+        img.style.height = '100vh';
+        img.style.width = '100vw';
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(hideGif, 5000);
+}
+function hideGif(){
+        overlay.style.display = 'none';
+        img.src = '';
+        clearTimeout(timeoutId);
+}
+
+
+function showShit(){
+    if (getRandomPercentage(5)) {
+        console.log("helo motherfucker");
+        showGif();
+    };
+    if (getRandomPercentage(2)) {
+        showPopup();
+    };
+};
+
+
+document.addEventListener("DOMContentLoaded", showShit);
